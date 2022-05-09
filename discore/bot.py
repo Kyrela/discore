a8ccf52225112a282d73e7952abe20edd97aaafa
Kyrela <mathis.vinay@gmail.com>
2022-05-09 22:46:02 +0200

    ➕ Added automatic cog load

--- discore/bot.py
+++ discore/bot.py
@@ -4,6 +4,7 @@ The class representing the Discord bot
 
 __all__ = ('Bot',)
 
+import os
 from os import path
 import toml
 import addict
@@ -18,13 +19,20 @@ class Bot(commands.Bot):
     The class representing the Discord bot
     """
 
-    def __init__(self, configuration_file="config.toml"):
+    class NoSpecifiedTokenError(Exception):
+        """
+        a basic custom error, in case no token is specified
+        """
+        pass
+
+    def __init__(self, configuration_file: str = "config.toml"):
         """
         Initialize the bot
+
+        :param configuration_file: the toml file containing configuration information
         """
 
-        default_config = toml.load(path.join(path.dirname(__file__), "default_config.toml"))
-        self.config = addict.Dict(merge(default_config, toml.load(configuration_file)))
+        self.load_config(configuration_file)
 
         intents = discord.Intents.all()
 
@@ -35,6 +43,34 @@ class Bot(commands.Bot):
             intents=intents
         )
 
+        self.load_cogs()
+
+    def load_config(self, configuration_file: str):
+        """
+        The configuration file loader
+
+        :param configuration_file: the path to the configuration file
+        """
+        default_config = toml.load(path.join(path.dirname(__file__), "default_config.toml"))
+        self.config = addict.Dict(merge(default_config, toml.load(configuration_file)))
+
+    def load_cogs(self):
+        """
+        loads dynamically the cogs found in the /cog folder
+        """
+        if path.isdir("cogs"):
+            for file in os.listdir("cogs"):
+                if file[-3:] == ".py":
+                    cog = file[:-3]
+                    exec(f"from cogs.{cog} import {cog.title()}")
+                    new_cog = eval(f"{cog.title()}(self)")
+                    self.add_cog(new_cog)
+                    if self.config.help.meta.cog.title() == cog.title():
+                        exec("self.help_command.cog = new_cog")
+        if self.config.help.meta.cog and not self.help_command.cog:
+            raise ModuleNotFoundError(
+                f"The cog {repr(self.config.help.meta.cog)}, required by the help command, wasn't found")
+
     def run(self):
         """
         Runs the bot
@@ -42,7 +78,10 @@ class Bot(commands.Bot):
         :return: None
         """
 
-        commands.Bot.run(self, self.config.token)
+        if 'token' in self.config:
+            commands.Bot.run(self, self.config.token)
+        else:
+            raise self.NoSpecifiedTokenError("No token is specified in the configuration file")
 
     def __enter__(self):
         return self
