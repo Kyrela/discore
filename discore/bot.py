250dc59c7c0bb52e5314d545e802eaa1c1d44ab0
Kyrela <mathis.vinay@gmail.com>
2022-05-11 00:11:48 +0200

    🔠 Enhanced code cleanliness consequences: 1 bug resolved, customization possibilities and compatibility with discord.py code enhanced

--- discore/bot.py
+++ discore/bot.py
@@ -14,6 +14,17 @@ from discord.ext import commands
 import discord
 
 
+def load_config(configuration_file: str) -> addict.Dict:
+    """
+    The configuration file loader
+
+    :param configuration_file: the path to the configuration file
+    :return: the configuration as an addict.Dict
+    """
+    default_config = toml.load(path.join(path.dirname(__file__), "default_config.toml"))
+    return addict.Dict(merge(default_config, toml.load(configuration_file)))
+
+
 class Bot(commands.Bot):
     """
     The class representing the Discord bot
@@ -25,63 +36,61 @@ class Bot(commands.Bot):
         """
         pass
 
-    def __init__(self, configuration_file: str = "config.toml"):
+    def __init__(self, configuration_file: str = "config.toml", **kwargs):
         """
         Initialize the bot
 
         :param configuration_file: the toml file containing configuration information
         """
 
-        self.load_config(configuration_file)
+        self.config = load_config(configuration_file)
 
-        intents = discord.Intents.all()
-
-        commands.Bot.__init__(
-            self,
-            command_prefix=self.config.prefix,
-            description=self.config.description if 'description' in self.config else None,
-            intents=intents
+        super().__init__(
+            command_prefix=kwargs.pop('command_prefix', False) or self.config.prefix,
+            description=kwargs.pop('description', False) or self.config.description or None,
+            intents=kwargs.pop('intents', False) or discord.Intents.all(),
+            **kwargs
         )
 
         self.load_cogs()
-
-    def load_config(self, configuration_file: str):
-        """
-        The configuration file loader
-
-        :param configuration_file: the path to the configuration file
-        """
-        default_config = toml.load(path.join(path.dirname(__file__), "default_config.toml"))
-        self.config = addict.Dict(merge(default_config, toml.load(configuration_file)))
 
     def load_cogs(self):
         """
         loads dynamically the cogs found in the /cog folder
         """
+
         if path.isdir("cogs"):
             for file in os.listdir("cogs"):
-                if file[-3:] == ".py":
-                    cog = file[:-3]
-                    exec(f"from cogs.{cog} import {cog.title()}")
-                    new_cog = eval(f"{cog.title()}(self)")
-                    self.add_cog(new_cog)
-                    if self.config.help.meta.cog.title() == cog.title():
-                        exec("self.help_command.cog = new_cog")
+
+                if not file.endswith(".py"):
+                    continue
+
+                cog_name = file[:-3]
+                exec(f"from cogs.{cog_name} import {cog_name.title()}")
+                new_cog = eval(f"{cog_name.title()}(self)")
+                self.add_cog(new_cog)
+
+                help_cog_name = self.config.help.meta.cog or None
+                if help_cog_name and help_cog_name.title() == cog_name.title():
+                    self.help_command.cog = new_cog
+
         if self.config.help.meta.cog and not self.help_command.cog:
             raise ModuleNotFoundError(
                 f"The cog {repr(self.config.help.meta.cog)}, required by the help command, wasn't found")
 
-    def run(self):
+    def run(self, token=None, **kwargs):
         """
         Runs the bot
 
         :return: None
         """
 
-        if 'token' in self.config:
-            commands.Bot.run(self, self.config.token)
-        else:
+        token = token or self.config.token or None
+
+        if not token:
             raise self.NoSpecifiedTokenError("No token is specified in the configuration file")
+
+        super().run(self.config.token, **kwargs)
 
     def __enter__(self):
         return self
