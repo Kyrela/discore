97dc838435cf0c040cee7c0ad28c6906dcf4d679
Kyrela <mathis.vinay@gmail.com>
2022-07-03 16:08:10 +0200

    🔧 Fixed typehint for Cog.bot

--- discore/cog.py
+++ discore/cog.py
@@ -3,6 +3,7 @@ A simple and empty cog, pre-attributed
 """
 
 from discord.ext import commands
+from .bot import Bot
 
 __all__ = ('Cog',)
 
@@ -12,6 +13,6 @@ class Cog(commands.Cog):
     creates an empty cog with a pre-attributed instance of the bot
     """
 
-    def __init__(self, bot: commands.Bot):
-        self.bot: commands.Bot = bot
+    def __init__(self, bot: Bot):
+        self.bot: Bot = bot
         super().__init__()
