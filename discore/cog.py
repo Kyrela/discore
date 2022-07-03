4efdbe1fcccb3fe769b77433d199d0487ba985e2
Kyrela <mathis.vinay@gmail.com>
2022-07-03 16:01:15 +0200

    ➕ Added typehint for Cog.bot

--- discore/cog.py
+++ discore/cog.py
@@ -12,6 +12,6 @@ class Cog(commands.Cog):
     creates an empty cog with a pre-attributed instance of the bot
     """
 
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
         super().__init__()
