8ba6c89ea0489972badd25bf06e48d9ee3348ba6
Kyrela <44508673+Kyrela@users.noreply.github.com>
2023-01-18 12:30:09 +0100

    🔧 syntax fix

--- discore/utils.py
+++ discore/utils.py
@@ -314,7 +314,7 @@ i18n.set('file_format', 'yml')
 i18n.set('locale', 'en-US')
 i18n.set('fallback', 'en-US')
 i18n.load_path.append(path.join(path.dirname(__file__), "locales"))
- if path.exists("locales") and path.isdir("locales"):
+if path.exists("locales") and path.isdir("locales"):
     i18n.load_path.append("locales")
 
 
