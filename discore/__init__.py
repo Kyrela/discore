50a2633b5d41505082169dfa03549f8664d21946
Kyrela <mathis.vinay@gmail.com>
2023-05-07 21:37:57 +0200

    📄 bump to 0.3

--- discore/__init__.py
+++ discore/__init__.py
@@ -1,7 +1,7 @@
 """
 Initialize the package Discore, based on the library discord.py.
 
-Copyright (c) 2022 Kyrela
+Copyright (c) 2022-2023 Kyrela
 """
 
 from discord import *
@@ -12,3 +12,12 @@ from discord.ext.tasks import *
 from .utils import *
 from .bot import Bot
 from .cog import Cog
+
+__version__ = "0.3"
+__copyright__ = "Copyright 2022-2023"
+__author__ = "Kyrela"
+__license__ = "MIT"
+__name__ = "Discore"
+__description__ = \
+    "A core for initialise, run and tracks errors of discord.py bots"
+__url__ = "https://github.com/kyrela/discore"
